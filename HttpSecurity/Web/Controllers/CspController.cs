b9205318a05c60fc10ac016b10442fa361affbaa
unknown <john.solem@R9EKM5T.bouvet.no>
2017-04-24 20:07:02 +0200

    First commit

---
+++ HttpSecurity/Web/Controllers/CspController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class CspController : Controller
+    {
+        // GET: Csp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(NoStore = true, Duration = 0)]
+        public ActionResult ImageSource(string src = "*")
+        {
+            Response.AddHeader("Content-Security-Policy", String.Format("image-src '{0}'", src));
+            ViewBag.Source = src;
+            return View();
+        }
+    }
+}
