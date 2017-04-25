2297dc81ecfe41d0529984bab19cc1efffcc9cef
JohnSverreSolem <john.solem@bouvet.no>
2017-04-25 22:52:39 +0200

    Arbeid med Csp

--- HttpSecurity/Web/Controllers/CspController.cs
+++ HttpSecurity/Web/Controllers/CspController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,11 +16,57 @@ namespace Web.Controllers
         }
 
         [OutputCache(NoStore = true, Duration = 0)]
-        public ActionResult ImageSource(string src = "*")
+        public ActionResult ImageSource(CspSource src = CspSource.None)
         {
-            Response.AddHeader("Content-Security-Policy", String.Format("image-src '{0}'", src));
+            Response.AddHeader("Content-Security-Policy", String.Format("img-src {0};", GetSource(src, "media.giphy.com")));
             ViewBag.Source = src;
+
             return View();
+        }
+
+        [Flags]
+        public enum CspSource
+        {
+            None = 1,
+            Self = 2,
+            Url = 4,
+            Data = 8,
+            All = 16,
+            SelfUrl = Self | Url,
+            SelfData = Self | Data,
+            SelfDataUrl  = Self | Data | Url
+        }
+
+        private string GetSource(CspSource source, string url = "www.example.com")
+        {
+            List<string> sources = new List<string>();
+
+            if (source.HasFlag(CspSource.None))
+            {
+                return "'none'";
+            }
+
+            if (source.HasFlag(CspSource.All))
+            {
+                return "'*'";
+            }
+
+            if (source.HasFlag(CspSource.Self))
+            {
+                sources.Add("'self'");
+            }
+
+            if (source.HasFlag(CspSource.Url))
+            {
+                sources.Add(url);
+            }
+
+            if (source.HasFlag(CspSource.Data))
+            {
+                sources.Add("data:");
+            }
+
+            return String.Join(" ", sources);
         }
     }
 }
